53d44590fea9c0a91ab7c129cecf73f6e409624d
youngji Jang <btg1631@naver.com>
2023-12-14 09:57:21 +0900

    mongo_insert_functions

--- docs/quests/mongo_insert_functions.py
+++ docs/quests/mongo_insert_functions.py
@@ -3,20 +3,13 @@ from pymongo import MongoClient
 # "mongodb://localhost:27017"
 
 def Connect_Mongo(localhost):
-
     mongoClient = MongoClient(localhost)
-
     database = mongoClient["local"]
-
     return database['fruit_info']
 
-
 def insert(insert_fruit):
-
     collection = Connect_Mongo("mongodb://localhost:27017")
-
     collection.insert_one(insert_fruit)
-
 
 insert({"name": "사과", "color": "빨간색", "origin": "한국"})
 insert({"name": "바나나", "color": "노란색", "origin": "필리핀"})
