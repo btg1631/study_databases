11540b19e719f11e818328f4272350be258767a4
youngji Jang <btg1631@naver.com>
2023-12-14 10:34:35 +0900

    mongo_insert_functions

--- docs/quests/mongo_insert_functions.py
+++ docs/quests/mongo_insert_functions.py
@@ -1,21 +1,26 @@
 from pymongo import MongoClient
 
-# "mongodb://localhost:27017"
-
 def Connect_Mongo(localhost):
-    mongoClient = MongoClient(localhost)
-    database = mongoClient["local"]
-    return database['fruit_info']
+    mongoClient = MongoClient(localhost)    # mongodb 접속
+    database = mongoClient["local"]         # database 연결
+    return database['fruit_info1']          # collection 작업
 
+# insert 작업
 def insert(insert_fruit):
-    collection = Connect_Mongo("mongodb://localhost:27017")
     collection.insert_one(insert_fruit)
 
-insert({"name": "사과", "color": "빨간색", "origin": "한국"})
-insert({"name": "바나나", "color": "노란색", "origin": "필리핀"})
-insert({"name": "딸기", "color": "빨간색", "origin": "한국"})
-insert({"name": "수박", "color": "초록색", "origin": "한국"})
-insert({"name": "오렌지", "color": "주황색", "origin": "미국"})
 
+fruit_info = [
+    {"name": "사과", "color": "빨간색", "origin": "한국"},
+    {"name": "바나나", "color": "노란색", "origin": "필리핀"},
+    {"name": "딸기", "color": "빨간색", "origin": "한국"},
+    {"name": "수박", "color": "초록색", "origin": "한국"},
+    {"name": "오렌지", "color": "주황색", "origin": "미국"},
+]
+
+collection = Connect_Mongo("mongodb://localhost:27017")
+
+for num in range(len(fruit_info)):
+    insert(fruit_info[num])
 
 
