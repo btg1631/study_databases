c3d788bb7c5e3552adc448f5d496f0ca596997f8
youngji Jang <btg1631@naver.com>
2024-03-29 15:33:22 +0900

    push

--- docs/sqls/selectwhereRangeAndEquals.sql
+++ docs/sqls/selectwhereRangeAndEquals.sql
@@ -6,5 +6,8 @@ FROM Customers
 -- Table Customers 
 -- 조건 Country 가 Germany 와 USA가 아닌 것들
 WHERE Country NOT IN ('Germany', 'USA');
+-- COUNT : 67
+
 -- 조건 : CustomerID가 50에서 89이고, City가 Berlin인 고객
 WHERE CustomerID BETWEEN 50 AND 89 AND CITY = 'London';
+-- COUNT : 2
